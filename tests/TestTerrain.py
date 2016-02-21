64046605bf7e7d7e7c57084cd0d64532de10d81f
Shreyas Iyer <Shreyas.Srinivasan@iiitb.org>
2016-02-21 15:01:35 +0530

    Used Display List to run the code 1000x smoother and faster.

--- tests/TestTerrain.py
+++ tests/TestTerrain.py
@@ -12,13 +12,16 @@ if __name__ == "__main__":
     play = Player(0,20,0)
     play.setOrigin(-10,-10,-50)
     moveList = [0,0,0]
-    keymap = {'up':[0,0,15],'down':[0,0,-15],'left':[15,0,0],'right':[-15,0,0]}
+    glNewList(12, GL_COMPILE)
+    terrain.render(ID)
+    glEndList()
+    keymap = {'up':[0,0,1],'down':[0,0,-1],'left':[1,0,0],'right':[-1,0,0]}
     while True:
         KeyboardEvent(moveList,keymap)
         window.clear()
         light0 = Light(30,50,20,[1,1,1,1],1)
         light0.bake(GL_LIGHT0)
         play.move(moveList[0],moveList[1],moveList[2])
-        terrain.render(ID)
+        glCallList(12)
         clock.tick(60)
         window.update()
